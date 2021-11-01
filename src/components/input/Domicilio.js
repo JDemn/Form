23d5c6926672c1587c6249385e9000c99ef59b2e
JDemn <dsoftwaremancilla@outlook.es>
2021-11-01 11:21:11 -0600

    button desplegable

--- src/components/input/Domicilio.js
+++ src/components/input/Domicilio.js
@@ -1,62 +1,68 @@
 import React, { useState } from 'react'
+import { FaArrowDown } from 'react-icons/fa';
 
 export const Domicilio = () => {
     const [show, setshow] = useState(false);
 
-    const addOptions = () => {
-        setshow(!show);
+    const addOptions = ({ currentTarget }) => {
+        currentTarget.className === 'domicilio__main pointer mt-3' && setshow(!show);
     }
 
     return (
-        <div className="domicilio__main pointer" onClick={addOptions}>
-            {!show && <h3>Domicilio</h3>}  
-            {/* Añadir el evento on click solo en el incono de flecha */}
+        <>
+            <div className="domicilio__main pointer mt-3" onClick={addOptions} >
+                {!show && <FaArrowDown className="domicilio__icon" />}
+                <h2>Domicilio</h2>
+            </div>
             {
                 show
                 &&
-                <>
-                    <input
-                        type="text"
-                        placeholder="Calle o avenida"
-                        name="domicilio"
-                    />
-                    <input
-                        type="number"
-                        placeholder="Número exterior"
-                        name="exterior"
-                    />
-                    <input
-                        type="numer"
-                        placeholder="Número interior"
-                        name="interior"
-                    />
-                    <input
-                        type="number"
-                        placeholder="Código postal"
-                        name="cp"
-                    />
-                    <input
-                        type="text"
-                        placeholder="Colonia o urbanización"
-                        name="colonia"
-                    />
-                    <input
-                        type="text"
-                        placeholder="Ciudad o población"
-                        name="ciudad"
-                    />
-                    <input
-                        type="text"
-                        placeholder="Entidad federativa o estado"
-                        name="entidad"
-                    />
-                    <input
-                        type="text"
-                        placeholder="País"
-                        name="pais"
-                    />
-                </>
+                <div className="domicilio__container">
+                    <>
+                        <input
+                            type="text"
+                            placeholder="Calle o avenida"
+                            name="domicilio"
+                        />
+                        <input
+                            type="number"
+                            placeholder="Número exterior"
+                            name="exterior"
+                        />
+                        <input
+                            type="number"
+                            placeholder="Número interior"
+                            name="interior"
+                        />
+                        <input
+                            type="number"
+                            placeholder="Código postal"
+                            name="cp"
+                        />
+                        <input
+                            type="text"
+                            placeholder="Colonia o urbanización"
+                            name="colonia"
+                        />
+                        <input
+                            type="text"
+                            placeholder="Ciudad o población"
+                            name="ciudad"
+                        />
+                        <input
+                            type="text"
+                            placeholder="Entidad federativa o estado"
+                            name="entidad"
+                        />
+                        <input
+                            type="text"
+                            placeholder="País"
+                            name="pais"
+                        />
+                    </>
+                </div>
             }
-        </div>
+
+        </>
     )
 }
--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -9,7 +9,6 @@ export const Input = () => {
                 <label>Razón Social</label>
                 <input
                     type="text"
-                    placeholder="Razón social"
                     name="rsocial"
                     className="form-input-gral"
                 />
@@ -17,13 +16,11 @@ export const Input = () => {
                 <label>Nombre comercial</label>
                 <input
                     type="text"
-                    placeholder="Nombre comercial"
                     name="ncomercial"
                 />
                 <label>Nacionalidad</label>
                 <input
                     type="text"
-                    placeholder="Nacionalidad"
                     name="nacionalidad"
                 />
                 <label>Fecha de constitución</label>
@@ -35,19 +32,16 @@ export const Input = () => {
                 <label>RFC</label>
                 <input
                     type="text"
-                    placeholder="Escriba su RFC"
                     name="rfc"
                 />
                 <label>Régimen fiscal</label>
                 <input
                     type="text"
-                    placeholder="regimen fiscal"
                     name="rfc"
                 />
                 <label>Industria</label>
                 <input
                     type="text"
-                    placeholder="industria"
                     name="rfc"
                 />
                 <Domicilio/>
