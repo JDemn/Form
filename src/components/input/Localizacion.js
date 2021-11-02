e7aacdb5434163ac0b2f9005d71f54c88fe076f1
JDemn <dsoftwaremancilla@outlook.es>
2021-11-01 22:14:18 -0600

    show msj of err when the email will be wrong

--- src/components/input/Localizacion.js
+++ src/components/input/Localizacion.js
@@ -2,30 +2,38 @@ import React from 'react'
 import { useEmail } from '../../hooks/useEmail'
 
 export const Localizacion = () => {
-
-    const {emailValidator} = useEmail();
+    
+    const { emailValidator, esCorrecto } = useEmail();
+    console.log('Es correcto?',esCorrecto);
 
     return (
-        <div className = "localizacion__main mt-4">
+        <div className="localizacion__main mt-4">
             <label>Comprobante de domicilio</label>
-            <input 
-                type = "file"
-                name = "fileComprobante"
-                className = "localizacion__input mb-2"
+            <input
+                type="file"
+                name="fileComprobante"
+                className="localizacion__input mb-2"
             />
             <label>Número de teléfono</label>
-            <input 
-                type = "number"
-                placeholder = "..."
-                name = "telefono"
+            <input
+                type="number"
+                placeholder="..."
+                name="telefono"
             />
             <label>Dirección de correo electrónico</label>
-            <input 
-                type = "email"
-                placeholder = "juan@example.com"
-                name = "email"
-                onChange = { emailValidator }
+            <input
+                type="email"
+                placeholder="juan@example.com"
+                name="email"
+                onChange={emailValidator}
             />
+            {   
+                esCorrecto === ''&&<h1></h1>,
+                esCorrecto===false&&
+                <div className="alert alert-danger mt-1" role="alert">
+                    Formato de contraseña incorrecto
+                </div>
+            }
         </div>
     )
 }
--- src/hooks/useEmail.js
+++ src/hooks/useEmail.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import { useState } from "react";
 
 export const useEmail = () => {
+    const [esCorrecto, setesCorrecto] = useState();
+
     const emailValidator = ({ target }) => {
         const regexEmail = /^[a-z][\w.-]+@\w[\w.-]+\.[\w.-]*[a-z][a-z]$/i;
+
         console.log(regexEmail.test(target.value));
+        setesCorrecto(regexEmail.test(target.value));
     }
 
     return {
-        emailValidator
+        emailValidator,
+        esCorrecto
     }
 }
